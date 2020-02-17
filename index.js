fab569c2b6b982148bfe4fb63dccae2690e24b0b
JackMarx <seriousfools@gmail.com>
2020-02-17 10:35:39 -0600

    babel

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
+var moment = require('moment');
+var math = require('mathjs');
+
 console.log("Hello from JS");
 
 var datetime = moment().startOf('hour').fromNow();
 console.log(datetime);
+
+console.log("Ch-Ch-Ch-changes");
+console.log(math.sqrt(-4));
+
+var name = 'Bob';
+var time = 'today';
+console.log(`Hello ${name}, how are you ${time}?`);
